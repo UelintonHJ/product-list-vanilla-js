31ff37a0d7ccdcc5365d6d0532f86c222fa59422
Uelinton Janke <uelinton.h@gmail.com>
2026-06-17 15:49:49 -0300

    feat(ui): modernize product catalog interface

--- js/components/ProductCard.js
+++ js/components/ProductCard.js
@@ -1,11 +1,25 @@
-export function ProductCard({title, price, image}) {
-    const card = document.createElement('div');
+export function ProductCard({ title, price, image }) {
+    const card = document.createElement('article');
+
     card.classList.add('card');
 
+    const formattedPrice =
+        new Intl.NumberFormat(
+            'pt-BR',
+            {
+                style: 'currency',
+                currency: 'BRL'
+            }
+        ).format(price);
+
     card.innerHTML = `
-    <img src="${image}" alt="${title}">
+    <div class="card-image">
+        <img src="${image}" alt="${title}">
+    </div>
+
     <h3>${title}</h3>
-    <p>R$ ${price}</p>
+    
+    <p class="price">${formattedPrice}</p>
     `;
 
     return card;
